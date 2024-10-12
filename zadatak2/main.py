39e55b0e34378b3a72efa0ecb7ae67bd027d7793
ItzMeTheAcoustism <milicamartic2023@gmail.com>
2024-10-12 10:53:04 +0200

    dodat novi zadatak

---
+++ zadatak2/main.py
@@ -0,0 +1,8 @@
+import math
+from racunajb import racunajB
+a = int(input("Kako si danas,zapravo briga me reci mi stranicu a "))
+d = int(input("ovde trebas staviti dijagonalu d "))
+b = racunajB(a,d)
+print("stranica be je",b,)
+
+
---
+++ zadatak2/racunajb.py
@@ -0,0 +1,4 @@
+import math
+def racunajB (a,d):
+    b = math.sqrt(d**2 - a**2)
+    return b
